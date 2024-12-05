a53863802b4caef1a5421663fe97799954c17a84
balantzy <balantzy@gmail.com>
2024-12-05 17:31:24 -0500

    fourth commit

--- app.js
+++ app.js
@@ -1,49 +1,29 @@
 const express = require('express');
 const app = express();
-const { products } = require('./data.js');
+const logger = require('./logger');
+const authorize = require('./authorize');
+
+// app.use(logger, authorize);
 
 app.get('/', (req, res) => {
-    res.send('<h1> Home Page </h1><a href="/api/products">products</a>');
+    res.send('Home');
 });
 
- app.get('/api/products', (req, res) => {
-     const newProducts = products.map((product) => {
-         const { id, name, image } = product;
-         return { id, name, image };
-     })
-     res.json(newProducts);
- })
+app.get('/about', (req, res) => {
+    res.send('About');
+});
 
-app.get('/api/products/:productID', (req, res) => {
-    const { productID } = req.params;
-    const singleProduct = products.find((product) => product.id === Number(productID));
-    if(!singleProduct){
-        return res.status(404).send('Product does not exist...')
-    }
-    res.json(singleProduct);
-})
+app.get('/api/products', (req, res) => {
+    res.send('Products');
+});
 
-app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
-    res.send('hello world');
-})
+app.get('/api/items', [logger, authorize], (req, res) => {
+    console.log(req.user);
+    res.send('Items');
+});
 
-app.get('/api/v1/query', (req, res) => {
-    const { search, limit } = req.query;
-    let sortedProducts = [...products];
 
-    if (search){
-        sortedProducts = sortedProducts.filter((product) => {
-            return product.name.startsWith(search);
-        })
-    }
-    if (limit){
-        sortedProducts = sortedProducts.slice(0, Number(limit));
-    }
-    if (sortedProducts.length < 1) {
-        return res.status(200).json({ success: true, data: [] });
-    }
-    return res.status(200).json(sortedProducts);
-})
+
 
 app.listen(5000, () => {
     console.log('Server is listening on PORT: 5000');
