dcb8ceed1dfb4419c5d98f6f985a18ae76aa1c86
balantzy <balantzy@gmail.com>
2024-12-06 07:04:27 -0500

    fifth commit

---
+++ 2-express-tutorial/6-middleware.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const logger = require('./logger');
+const authorize = require('./authorize');
+
+// app.use(logger, authorize);
+
+app.get('/', (req, res) => {
+    res.send('Home');
+});
+
+app.get('/about', (req, res) => {
+    res.send('About');
+});
+
+app.get('/api/products', (req, res) => {
+    res.send('Products');
+});
+
+app.get('/api/items', [logger, authorize], (req, res) => {
+    console.log(req.user);
+    res.send('Items');
+});
+
+
+
+
+app.listen(5000, () => {
+    console.log('Server is listening on PORT: 5000');
+})
