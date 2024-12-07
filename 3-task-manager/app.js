79546af6998b90b81679ef853f559fcc883b7434
balantzy <balantzy@gmail.com>
2024-12-07 14:43:34 -0500

    commit #8 - task manager functional

--- 3-task-manager/app.js
+++ 3-task-manager/app.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config();
 
 // middleware
-
+app.use(express.static('./public'))
 app.use(express.json());
 
 // routes
-app.get('/hello', (req, res) => {
-    res.send('Task Manager App');
-})
 
 app.use('/api/v1/tasks', tasks);
 
 const port = 3000;
 
-app.listen(port, console.log(`Port is listening on ${port}...`));
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, console.log(`Server is listening on PORT: ${port}...`));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
